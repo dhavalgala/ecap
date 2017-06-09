000f5e3ee86574952ca0ec3125192ed2812ef756
Dhaval Gala <dhaval.gala59@gmail.com>
2017-06-09 11:36:06 +0530

    move category name up

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -11,31 +11,24 @@ export class AppService {
     // private serverUrl = "http://halb.tk/admin/index.php/";
     private serverUrl = "http://www.ecap.net.in/admin/index.php/";
 
-    private headers = new Headers();
-
-    constructor(private _http: Http) {
-        this.headers.append('Accept', 'application/json');
-        this.headers.append('Content-Type', 'application/json');
-    }
+    constructor(private _http: Http) { }
 
     getSliderByCategory() {
-        return this._http.get(this.serverUrl + "json/getAllSliderWithoutPagination", { headers: this.headers })
+        return this._http.get(this.serverUrl + "json/getAllSliderWithoutPagination")
             .map(res => res.json());
     }
 
     getAboutUs() {
-        return this._http.get(this.serverUrl + "json/getAboutUsContent", { headers: this.headers })
+        return this._http.get(this.serverUrl + "json/getAboutUsContent")
             .map(res => res.json());
     }
 
     getAllCategory() {
-        return this._http.get(this.serverUrl + "json/getAllCategory", { headers: this.headers })
+        return this._http.get(this.serverUrl + "json/getAllCategory")
             .map(res => res.json());
     }
 
     submitEnquiry(obj: any) {
-        return this._http.post(this.serverUrl + "json/getContactInfo", obj,
-            { headers: this.headers }).map(res => res.json());
+        return this._http.post(this.serverUrl + "json/getContactInfo", obj).map(res => res.json());
     }
-
 }
